1b121d54a06c51b0c68aef6426a7c91fa43f2b3a
Mingzhi Zheng <mizheng@microsoft.com>
2018-11-19 19:39:34 -0800

    update model util to support new seq lm model

--- language_model/util/model_util.py
+++ language_model/util/model_util.py
@@ -3,30 +3,26 @@ import collections
 import numpy as np
 import tensorflow as tf
 
-from model.language_model import *
-from model.language_model_bidirectional import *
-from util.default_util import *
+from model.seq_lm import *
 from util.data_util import *
-from util.language_model_util import *
 
-__all__ = ["TrainModel", "EvalModel", "InferModel", "EncodeModel",
-           "create_train_model", "create_eval_model", "create_infer_model", "create_encode_model",
-           "get_model_creator", "init_model", "load_model"]
+__all__ = ["TrainModel", "EvalModel", "DecodeModel", "EncodeModel",
+           "create_train_model", "create_eval_model", "create_decode_model", "create_encode_model",
+           "init_model", "load_model"]
 
-class TrainModel(collections.namedtuple("TrainModel",
-    ("graph", "model", "data_pipeline", "embedding"))):
+class TrainModel(collections.namedtuple("TrainModel", ("graph", "model", "data_pipeline", "word_embedding"))):
     pass
 
 class EvalModel(collections.namedtuple("EvalModel",
-    ("graph", "model", "data_pipeline", "embedding"))):
+    ("graph", "model", "data_pipeline", "input_data", "word_embedding"))):
     pass
 
-class InferModel(collections.namedtuple("InferModel",
-    ("graph", "model", "data_pipeline", "input_data", "embedding"))):
+class DecodeModel(collections.namedtuple("DecodeModel",
+    ("graph", "model", "data_pipeline", "input_data", "word_embedding"))):
     pass
 
 class EncodeModel(collections.namedtuple("EncodeModel",
-    ("graph", "model", "data_pipeline", "input_data", "embedding"))):
+    ("graph", "model", "data_pipeline", "input_data", "word_embedding"))):
     pass
 
 def create_train_model(logger,
@@ -34,99 +30,149 @@ def create_train_model(logger,
     graph = tf.Graph()
     with graph.as_default():
         logger.log_print("# prepare train data")
-        (input_data, embedding_data, vocab_size, vocab_index,
-            vocab_inverted_index) = prepare_data(logger, hyperparams.data_train_file,
-            hyperparams.data_vocab_file, hyperparams.data_embedding_file, hyperparams.data_full_embedding_file,
-            hyperparams.data_vocab_size, hyperparams.model_embed_dim, hyperparams.data_unk, hyperparams.data_sos,
-            hyperparams.data_eos, hyperparams.data_pad, hyperparams.model_pretrained_embedding)
-    
+        (input_data, word_embed_data, word_vocab_size, word_vocab_index, word_vocab_inverted_index,  
+            char_vocab_size, char_vocab_index, char_vocab_inverted_index) = prepare_data(logger,
+            hyperparams.data_train_file, hyperparams.data_word_vocab_file, hyperparams.data_word_vocab_size,
+            hyperparams.data_word_vocab_threshold, hyperparams.model_word_embed_dim, hyperparams.data_embedding_file,
+            hyperparams.data_full_embedding_file, hyperparams.data_word_unk, hyperparams.data_word_pad, hyperparams.data_word_unk,
+            hyperparams.data_word_pad, hyperparams.model_word_feat_enable, hyperparams.model_word_embed_pretrained,
+            hyperparams.data_char_vocab_file, hyperparams.data_char_vocab_size, hyperparams.data_char_vocab_threshold,
+            hyperparams.data_char_unk, hyperparams.data_char_pad, hyperparams.model_char_feat_enable)
+        
+        logger.log_print("# create train text dataset")
+        text_dataset = tf.data.TextLineDataset([hyperparams.data_train_file])
+        input_text_word_dataset, input_text_char_dataset = create_text_dataset(text_dataset,
+            word_vocab_index, hyperparams.data_text_word_size, hyperparams.data_word_pad,
+            hyperparams.data_word_sos, hyperparams.data_word_eos, hyperparams.model_word_feat_enable,
+            char_vocab_index, hyperparams.data_text_char_size, hyperparams.data_char_pad, hyperparams.model_char_feat_enable)
+        
         logger.log_print("# create train data pipeline")
-        data_pipeline = create_data_pipeline(hyperparams.data_train_file, vocab_index,
-            hyperparams.data_max_length, hyperparams.data_sos, hyperparams.data_eos,
-            hyperparams.data_pad, hyperparams.train_batch_size, hyperparams.train_random_seed,
-            hyperparams.train_enable_shuffle, hyperparams.model_type)
+        data_pipeline = create_data_pipeline(input_text_word_dataset, input_text_char_dataset,
+            word_vocab_size, word_vocab_index, word_vocab_inverted_index, hyperparams.data_word_pad,
+            hyperparams.model_word_feat_enable, char_vocab_size, char_vocab_index, hyperparams.data_char_pad,
+            hyperparams.model_char_feat_enable, hyperparams.train_enable_shuffle, hyperparams.train_shuffle_buffer_size,
+            len(input_data), hyperparams.train_batch_size, hyperparams.train_random_seed)
         
         model_creator = get_model_creator(hyperparams.model_type)
         model = model_creator(logger=logger, hyperparams=hyperparams, data_pipeline=data_pipeline,
-            vocab_size=vocab_size, vocab_index=vocab_index, vocab_inverted_index=vocab_inverted_index,
             mode="train", scope=hyperparams.model_scope)
         
-        return TrainModel(graph=graph, model=model, data_pipeline=data_pipeline, embedding=embedding_data)
+        return TrainModel(graph=graph, model=model, data_pipeline=data_pipeline, word_embedding=word_embed_data)
 
 def create_eval_model(logger,
                       hyperparams):
     graph = tf.Graph()
     with graph.as_default():
         logger.log_print("# prepare eval data")
-        (input_data, embedding_data, vocab_size, vocab_index,
-            vocab_inverted_index) = prepare_data(logger, hyperparams.data_eval_file,
-            hyperparams.data_vocab_file, hyperparams.data_embedding_file, hyperparams.data_full_embedding_file,
-            hyperparams.data_vocab_size, hyperparams.model_embed_dim, hyperparams.data_unk, hyperparams.data_sos,
-            hyperparams.data_eos, hyperparams.data_pad, hyperparams.model_pretrained_embedding)
-    
+        (input_data, word_embed_data, word_vocab_size, word_vocab_index, word_vocab_inverted_index,  
+            char_vocab_size, char_vocab_index, char_vocab_inverted_index) = prepare_data(logger,
+            hyperparams.data_eval_file, hyperparams.data_word_vocab_file, hyperparams.data_word_vocab_size,
+            hyperparams.data_word_vocab_threshold, hyperparams.model_word_embed_dim, hyperparams.data_embedding_file,
+            hyperparams.data_full_embedding_file, hyperparams.data_word_unk, hyperparams.data_word_pad, hyperparams.data_word_unk,
+            hyperparams.data_word_pad, hyperparams.model_word_feat_enable, hyperparams.model_word_embed_pretrained,
+            hyperparams.data_char_vocab_file, hyperparams.data_char_vocab_size, hyperparams.data_char_vocab_threshold,
+            hyperparams.data_char_unk, hyperparams.data_char_pad, hyperparams.model_char_feat_enable)
+        
+        logger.log_print("# create eval text dataset")
+        text_placeholder = tf.placeholder(shape=[None], dtype=tf.string)
+        text_dataset = tf.data.Dataset.from_tensor_slices(text_placeholder)
+        input_text_word_dataset, input_text_char_dataset = create_text_dataset(text_dataset,
+            word_vocab_index, hyperparams.data_text_word_size, hyperparams.data_word_pad,
+            hyperparams.data_word_sos, hyperparams.data_word_eos, hyperparams.model_word_feat_enable,
+            char_vocab_index, hyperparams.data_text_char_size, hyperparams.data_char_pad, hyperparams.model_char_feat_enable)
+        
         logger.log_print("# create eval data pipeline")
-        data_pipeline = create_data_pipeline(hyperparams.data_eval_file, vocab_index,
-            hyperparams.data_max_length, hyperparams.data_sos, hyperparams.data_eos, hyperparams.data_pad,
-            hyperparams.train_eval_batch_size, hyperparams.train_random_seed, False, hyperparams.model_type)
+        data_size_placeholder = tf.placeholder(shape=[], dtype=tf.int64)
+        batch_size_placeholder = tf.placeholder(shape=[], dtype=tf.int64)
+        data_pipeline = create_dynamic_pipeline(input_text_word_dataset, input_text_char_dataset,
+            word_vocab_size, word_vocab_index, word_vocab_inverted_index, hyperparams.data_word_pad,
+            hyperparams.model_word_feat_enable, char_vocab_size, char_vocab_index, hyperparams.data_char_pad,
+            hyperparams.model_char_feat_enable, text_placeholder, data_size_placeholder, batch_size_placeholder)
         
         model_creator = get_model_creator(hyperparams.model_type)
         model = model_creator(logger=logger, hyperparams=hyperparams, data_pipeline=data_pipeline,
-            vocab_size=vocab_size, vocab_index=vocab_index, vocab_inverted_index=vocab_inverted_index,
             mode="eval", scope=hyperparams.model_scope)
         
-        return EvalModel(graph=graph, model=model, data_pipeline=data_pipeline, embedding=embedding_data)
+        return EvalModel(graph=graph, model=model, data_pipeline=data_pipeline,
+            input_data=input_data, word_embedding=word_embed_data)
 
-def create_infer_model(logger,
-                       hyperparams):
+def create_decode_model(logger,
+                        hyperparams):
     graph = tf.Graph()
     with graph.as_default():
-        logger.log_print("# prepare infer data")
-        (input_data, embedding_data, vocab_size, vocab_index,
-            vocab_inverted_index) = prepare_data(logger, hyperparams.data_eval_file,
-            hyperparams.data_vocab_file, hyperparams.data_embedding_file, hyperparams.data_full_embedding_file,
-            hyperparams.data_vocab_size, hyperparams.model_embed_dim, hyperparams.data_unk, hyperparams.data_sos,
-            hyperparams.data_eos, hyperparams.data_pad, hyperparams.model_pretrained_embedding)
-    
-        logger.log_print("# create infer data pipeline")
-        data_pipeline = create_dynamic_data_pipeline(vocab_index, hyperparams.data_max_length,
-            hyperparams.data_sos, hyperparams.data_eos, hyperparams.data_pad, hyperparams.model_type)
+        logger.log_print("# prepare decode data")
+        (input_data, word_embed_data, word_vocab_size, word_vocab_index, word_vocab_inverted_index,  
+            char_vocab_size, char_vocab_index, char_vocab_inverted_index) = prepare_data(logger,
+            hyperparams.data_eval_file, hyperparams.data_word_vocab_file, hyperparams.data_word_vocab_size,
+            hyperparams.data_word_vocab_threshold, hyperparams.model_word_embed_dim, hyperparams.data_embedding_file,
+            hyperparams.data_full_embedding_file, hyperparams.data_word_unk, hyperparams.data_word_pad, hyperparams.data_word_unk,
+            hyperparams.data_word_pad, hyperparams.model_word_feat_enable, hyperparams.model_word_embed_pretrained,
+            hyperparams.data_char_vocab_file, hyperparams.data_char_vocab_size, hyperparams.data_char_vocab_threshold,
+            hyperparams.data_char_unk, hyperparams.data_char_pad, hyperparams.model_char_feat_enable)
+        
+        logger.log_print("# create decode text dataset")
+        text_placeholder = tf.placeholder(shape=[None], dtype=tf.string)
+        text_dataset = tf.data.Dataset.from_tensor_slices(text_placeholder)
+        input_text_word_dataset, input_text_char_dataset = create_text_dataset(text_dataset,
+            word_vocab_index, hyperparams.data_text_word_size, hyperparams.data_word_pad,
+            hyperparams.data_word_sos, hyperparams.data_word_eos, hyperparams.model_word_feat_enable,
+            char_vocab_index, hyperparams.data_text_char_size, hyperparams.data_char_pad, hyperparams.model_char_feat_enable)
+        
+        logger.log_print("# create decode data pipeline")
+        data_size_placeholder = tf.placeholder(shape=[], dtype=tf.int64)
+        batch_size_placeholder = tf.placeholder(shape=[], dtype=tf.int64)
+        data_pipeline = create_dynamic_pipeline(input_text_word_dataset, input_text_char_dataset,
+            word_vocab_size, word_vocab_index, word_vocab_inverted_index, hyperparams.data_word_pad,
+            hyperparams.model_word_feat_enable, char_vocab_size, char_vocab_index, hyperparams.data_char_pad,
+            hyperparams.model_char_feat_enable, text_placeholder, data_size_placeholder, batch_size_placeholder)
         
         model_creator = get_model_creator(hyperparams.model_type)
         model = model_creator(logger=logger, hyperparams=hyperparams, data_pipeline=data_pipeline,
-            vocab_size=vocab_size, vocab_index=vocab_index, vocab_inverted_index=vocab_inverted_index,
-            mode="infer", scope=hyperparams.model_scope)
+            mode="decode", scope=hyperparams.model_scope)
         
-        return InferModel(graph=graph, model=model, data_pipeline=data_pipeline,
-            input_data=input_data, embedding=embedding_data)
+        return DecodeModel(graph=graph, model=model, data_pipeline=data_pipeline,
+            input_data=input_data, word_embedding=word_embed_data)
 
 def create_encode_model(logger,
                         hyperparams):
     graph = tf.Graph()
     with graph.as_default():
-        logger.log_print("# prepare encoding data")
-        (input_data, embedding_data, vocab_size, vocab_index,
-            vocab_inverted_index) = prepare_data(logger, hyperparams.data_eval_file,
-            hyperparams.data_vocab_file, hyperparams.data_embedding_file, hyperparams.data_full_embedding_file,
-            hyperparams.data_vocab_size, hyperparams.model_embed_dim, hyperparams.data_unk, hyperparams.data_sos,
-            hyperparams.data_eos, hyperparams.data_pad, hyperparams.model_pretrained_embedding)
-    
-        logger.log_print("# create encoding data pipeline")
-        data_pipeline = create_dynamic_data_pipeline(vocab_index, hyperparams.data_max_length,
-            hyperparams.data_sos, hyperparams.data_eos, hyperparams.data_pad, hyperparams.model_type)
+        logger.log_print("# prepare encode data")
+        (input_data, word_embed_data, word_vocab_size, word_vocab_index, word_vocab_inverted_index,  
+            char_vocab_size, char_vocab_index, char_vocab_inverted_index) = prepare_data(logger,
+            hyperparams.data_eval_file, hyperparams.data_word_vocab_file, hyperparams.data_word_vocab_size,
+            hyperparams.data_word_vocab_threshold, hyperparams.model_word_embed_dim, hyperparams.data_embedding_file,
+            hyperparams.data_full_embedding_file, hyperparams.data_word_unk, hyperparams.data_word_pad, hyperparams.data_word_unk,
+            hyperparams.data_word_pad, hyperparams.model_word_feat_enable, hyperparams.model_word_embed_pretrained,
+            hyperparams.data_char_vocab_file, hyperparams.data_char_vocab_size, hyperparams.data_char_vocab_threshold,
+            hyperparams.data_char_unk, hyperparams.data_char_pad, hyperparams.model_char_feat_enable)
+        
+        logger.log_print("# create encode text dataset")
+        text_placeholder = tf.placeholder(shape=[None], dtype=tf.string)
+        text_dataset = tf.data.Dataset.from_tensor_slices(text_placeholder)
+        input_text_word_dataset, input_text_char_dataset = create_text_dataset(text_dataset,
+            word_vocab_index, hyperparams.data_text_word_size, hyperparams.data_word_pad,
+            hyperparams.data_word_sos, hyperparams.data_word_eos, hyperparams.model_word_feat_enable,
+            char_vocab_index, hyperparams.data_text_char_size, hyperparams.data_char_pad, hyperparams.model_char_feat_enable)
+        
+        logger.log_print("# create encode data pipeline")
+        data_size_placeholder = tf.placeholder(shape=[], dtype=tf.int64)
+        batch_size_placeholder = tf.placeholder(shape=[], dtype=tf.int64)
+        data_pipeline = create_dynamic_pipeline(input_text_word_dataset, input_text_char_dataset,
+            word_vocab_size, word_vocab_index, word_vocab_inverted_index, hyperparams.data_word_pad,
+            hyperparams.model_word_feat_enable, char_vocab_size, char_vocab_index, hyperparams.data_char_pad,
+            hyperparams.model_char_feat_enable, text_placeholder, data_size_placeholder, batch_size_placeholder)
         
         model_creator = get_model_creator(hyperparams.model_type)
         model = model_creator(logger=logger, hyperparams=hyperparams, data_pipeline=data_pipeline,
-            vocab_size=vocab_size, vocab_index=vocab_index, vocab_inverted_index=vocab_inverted_index,
             mode="encode", scope=hyperparams.model_scope)
         
         return EncodeModel(graph=graph, model=model, data_pipeline=data_pipeline,
-            input_data=input_data, embedding=embedding_data)
+            input_data=input_data, word_embedding=word_embed_data)
 
 def get_model_creator(model_type):
-    if model_type == "forward_only":
-        model_creator = LanguageModel
-    elif model_type == "bi_directional":
-        model_creator = LanguageModelBidirectional
+    if model_type == "seq_lm":
+        model_creator = SequenceLM
     else:
         raise ValueError("can not create model with unsupported model type {0}".format(model_type))
     
@@ -139,6 +185,8 @@ def init_model(sess,
         sess.run(tf.tables_initializer())
 
 def load_model(sess,
-               model):
+               model,
+               ckpt_file,
+               ckpt_type):
     with model.graph.as_default():
-        model.model.restore(sess)
+        model.model.restore(sess, ckpt_file, ckpt_type)
